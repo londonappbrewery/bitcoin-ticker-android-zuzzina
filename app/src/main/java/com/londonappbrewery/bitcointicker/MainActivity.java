f42ab0d1a56e37637f0f9fd9c30407040ad658b6
londonappbrewery <londonappbrewery@gmail.com>
2016-03-30 16:58:44 +0100

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -0,0 +1,78 @@
+package com.londonappbrewery.bitcointicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constants:
+    private final String BASE_URL = "https://api.bitcoinaverage.com/ticker/";
+
+    // Member Variables:
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
+        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+
+        // Create an ArrayAdapter using the String array and a spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // TODO: Set an OnItemSelected listener on the spinner
+
+    }
+
+    // TODO: complete the letsDoSomeNetworking() method
+    private void letsDoSomeNetworking(String url) {
+
+//        AsyncHttpClient client = new AsyncHttpClient();
+//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
+//
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+//                // called when response HTTP status is "200 OK"
+//                Log.d("Clima", "JSON: " + response.toString());
+//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
+//                updateUI(weatherData);
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+//                Log.d("Clima", "Request fail! Status code: " + statusCode);
+//                Log.d("Clima", "Fail response: " + response);
+//                Log.e("ERROR", e.toString());
+//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+    }
+
+
+}
